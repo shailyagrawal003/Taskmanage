5b833c67d38ebf75c266e8ed4a7ae3dfc2ea47af
shaily_agrawal <shailyagrawal003@gmail.com>
2026-03-19 18:38:22 +0530

    updated css

--- taskapp/src/About.jsx
+++ taskapp/src/About.jsx
@@ -1,8 +1,7 @@
 function About(){
   return(
     <> 
-  <p>This application is designed to help users organize their daily tasks in a simple and efficient way.for adding new task in their app </p>
-  
+   <p>This application is designed to help users organize their daily tasks in a simple and efficient way.for adding new task in their app </p>
     </>
   )
 }
